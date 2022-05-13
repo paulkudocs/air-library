75b2be8740c7862f2648f998e266004d0c4da013
Paul Anthony McGowan <xhostcom@gmail.com>
2022-05-13 08:37:39 +0100

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reviews, defaults: { format: :json }
+  resources :books, defaults: { format: :json }
+  resources :users, defaults: { format: :json }
 end
